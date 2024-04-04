abada6c72f90b79174b22d5c0992afb230d2369f
whitedragon077 <112774207+whitedragon077@users.noreply.github.com>
2024-04-04 23:00:40 +0300

    Add files via upload

---
+++ src/test/java/com/example/cars/CarsApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.cars;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertTrue(true);
+    }
+
+}
