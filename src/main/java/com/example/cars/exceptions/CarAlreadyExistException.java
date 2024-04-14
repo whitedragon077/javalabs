cf7ec1bc37fee0c1cf0dd9ff9a72cf9cda399652
whitedragon077 <112774207+whitedragon077@users.noreply.github.com>
2024-04-14 18:03:30 +0300

    Delete src directory

--- src/main/java/com/example/cars/exceptions/CarAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package com.example.cars.exceptions;
-
-public class CarAlreadyExistException extends Exception{
-    public CarAlreadyExistException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/cars/repository/CarRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.cars.repository;
-
-import com.example.cars.entity.Car;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CarRepository extends CrudRepository<Car, Long> {
-    Car findByMake(String make);
-}
